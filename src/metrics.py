d33c4a5d157fd744d0b1f5fd480e212ca598a108
Prince Kumar <princenits2017@gmail.com>
2026-01-19 20:45:12 -0600

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,1 +1,8 @@
+from sklearn.metrics import r2_score, mean_absolute_error
+
+def regression_metrics(y_true, y_pred):
+    return {
+        "R2": r2_score(y_true, y_pred),
+        "MAE": mean_absolute_error(y_true, y_pred)
+    }
 
